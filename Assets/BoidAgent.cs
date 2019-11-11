89df52f8939b919532cffa3f9393d504027e1c36
Rafael <rvitall.alves@gmail.com>
2019-11-10 23:39:28 -0300

    initial implementation

---
+++ Assets/BoidAgent.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidAgent : MonoBehaviour
+{
+    public float speed = 10;
+    [SerializeField] private float cohesionDist = 2;
+    [SerializeField] private float avoidanceDist = 1;
+    [SerializeField] private float rotationSpeed = 1;
+    private GameObject[] mBoids;
+    private Vector3 avoidDir;
+    private Vector3 stirDir;
+    [SerializeField] private int neighbourCount;
+    private bool returning = false;
+
+    void Start()
+    {
+        mBoids = FlockManager.Instance.boids;
+    }
+
+    // private void OnDrawGizmos() {
+    //     Gizmos.color = new Color (1,1,1,0.1f);
+    //     Gizmos.DrawSphere (transform.position, cohesionDist);
+    // }
+    void Update()
+    {
+        // Gizmos.DrawSphere (transform.position, cohesionDist);
+        if (Vector3.Distance (transform.position, Vector3.zero) >= FlockManager.Instance.boundSize) returning = true;
+        else returning = false;
+
+        if (returning) {
+            Vector3 distance = Vector3.zero - transform.position;
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (distance), rotationSpeed * Time.deltaTime);
+        } else {
+            // if (Random.Range (0,5) < 1) ApplyRules ();
+            ApplyRules ();
+        }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+
+    private void ApplyRules () {
+        Vector3 cohesionDir = Vector3.zero;
+        Vector3 avgForward = Vector3.zero;
+        float distance = 0;
+        float avgSpeed = 0.1f;
+        neighbourCount = 0;
+        Vector3 goalPos = FlockManager.Instance.goalPos;
+
+        foreach (var boid in mBoids)
+        {
+            if (boid != gameObject) {
+
+                distance = Vector3.Distance (boid.transform.position, transform.position);
+            
+                if (distance <= cohesionDist) {
+                    neighbourCount ++;  
+                    Debug.DrawLine (transform.position, boid.transform.position, new Color (1,1,1,0.5f));
+                    cohesionDir += boid.transform.position;
+                    avgForward += boid.transform.forward;
+                    if (distance <= avoidanceDist) {
+                        avoidDir += transform.position - boid.transform.position;
+                    }
+
+                    avgSpeed += boid.GetComponent <BoidAgent> ().speed;
+                } 
+            }
+        }
+
+        if (neighbourCount > 0) {
+
+            cohesionDir = cohesionDir/neighbourCount + (goalPos - transform.position);
+            speed = avgSpeed / neighbourCount;
+            speed = Mathf.Clamp (speed, 0, 10);
+            stirDir = (cohesionDir + avoidDir + avgForward) - transform.position;
+            if (stirDir != Vector3.zero) {
+                transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (stirDir), Time.deltaTime * rotationSpeed);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/FlockManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager Instance;
+    private void Awake() {
+        if (Instance!= this) Destroy (Instance);
+        if (Instance == null) Instance =  this;
+    }
+    public static int boidNum;
+    public float boundSize = 5;
+    [HideInInspector] public Vector3 goalPos;
+    [SerializeField] private int _boidNum = 10;
+    [SerializeField] private float goalRefresh = 5;
+    public GameObject[] boids;
+    [SerializeField] private GameObject boidPrefab = null;
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boidNum = _boidNum;
+        boids = new GameObject[boidNum];
+        for (int i = 0; i < boidNum; i++)
+        {
+            GameObject currentBoid = (GameObject) Instantiate (boidPrefab, new Vector3 (
+                Random.Range (-boundSize, boundSize),
+                Random.Range (-boundSize, boundSize),
+                Random.Range (-boundSize, boundSize)
+            ), Random.rotation, transform);
+            boids[i] = currentBoid;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer+= Time.deltaTime;
+        if (timer >= goalRefresh) {
+            goalPos = Random.insideUnitSphere * Random.Range (1, boundSize);
+        }
+        // if (Random.Range (0,5) < 1)
+    }
+}
